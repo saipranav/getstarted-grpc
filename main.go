643134e99445197cf275624d6052bccf405b55cd
Sai Pranav <rsaipranav92@gmail.com>
2020-04-26 05:26:55 -0500

    pa: learnt a bit about protocol buffers

--- main.go
+++ main.go
@@ -2,8 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/saipranav/getstarted-grpc-go/store"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 )
 
+func echo(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		w.WriteHeader(http.StatusAccepted)
+		w.Write(b)
+	}
+}
+
+func storeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		en := &store.Entity{}
+		err = protojson.Unmarshal(b, en)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		data, err := proto.Marshal(en)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		w.WriteHeader(http.StatusAccepted)
+		w.Write(data)
+	}
+}
+
 func main() {
-	fmt.Println("asdf")
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/store", storeHandler)
+	http.ListenAndServe(":8080", nil)
 }
